46d87cb10bfcde1b27ca212320260fcd1e36b0dc
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-04-20 13:19:12 +0300

    - Update build - Switch to HashHistory to fix broken build

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import {Provider} from "react-redux";
 import {render} from 'react-dom';
 import React from "react";
 import store from './store';
-import mars from "../assets/images/mars.png";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const theme = createMuiTheme({
     typography: {
@@ -32,9 +31,9 @@ render(
     <div>
         <MuiThemeProvider theme={theme}>
             <Provider store={store}>
-                <BrowserRouter>
+                <HashRouter>
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </Provider>
         </MuiThemeProvider>
     </div>,
